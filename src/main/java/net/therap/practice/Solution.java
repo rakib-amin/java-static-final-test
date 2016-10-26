bc76f2e63e5481a983e7a1ad6af6d18770dba3c5
Mohammad Rakib Amin <rakib.amin@therapservices.net>
2016-10-26 23:37:15 +0600

    Relocated all Exceptions to the main method

--- src/main/java/net/therap/practice/Solution.java
+++ src/main/java/net/therap/practice/Solution.java
@@ -20,30 +20,18 @@ class Calculate{
     }
     // cube
     public double get_volume(int a){
-        if(a <= 0) {
-            throw new NumberFormatException("All the values must be positive");
-        }
         return a*a*a;
     }
     // cuboid
     public double get_volume(int l, int b, int h){
-        if(l <= 0 || b <= 0 || h <= 0) {
-            throw new NumberFormatException("All the values must be positive");
-        }
         return l*b*h;
     }
     // sphere
     public double get_volume(double r){
-        if(r <= 0) {
-            throw new NumberFormatException("All the values must be positive");
-        }
         return (2*Math.PI*r*r*r)/3.0;
     }
     // cyllinder
     public double get_volume(double r, double h){
-        if(r <= 0 || h <= 0) {
-            throw new NumberFormatException("All the values must be positive");
-        }
         return Math.PI*r*r*h;
     }
     public int get_int_val() throws IOException {
@@ -74,20 +62,32 @@ public class Solution {
                 int ch = cal.get_int_val();
                 if (ch == 1) {
                     int a = cal.get_int_val();
+                    if(a <= 0) {
+                        throw new NumberFormatException("All the values must be positive");
+                    }
                     volume = Calculate.do_calc().get_volume(a);
                 } else if (ch == 2) {
                     int l = cal.get_int_val();
                     int b = cal.get_int_val();
                     int h = cal.get_int_val();
+                    if(l <= 0 || b <= 0 || h <= 0) {
+                        throw new NumberFormatException("All the values must be positive");
+                    }
                     volume = Calculate.do_calc().get_volume(l, b, h);
 
                 } else if (ch == 3) {
                     double r = cal.get_double_val();
+                    if(r <= 0) {
+                        throw new NumberFormatException("All the values must be positive");
+                    }
                     volume = Calculate.do_calc().get_volume(r);
 
                 } else if (ch == 4) {
                     double r = cal.get_double_val();
                     double h = cal.get_double_val();
+                    if(r <= 0 || h <= 0) {
+                        throw new NumberFormatException("All the values must be positive");
+                    }
                     volume = Calculate.do_calc().get_volume(r, h);
 
                 }
